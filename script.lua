898d68221bfcae3c5720326dbdf52959a2b40fbe
IfkumRfnl <ifkumrfnl@gmail.com>
2024-06-24 17:04:09 +0400

    init

---
+++ script.lua
@@ -0,0 +1,22 @@
+local btn = get("btn")
+local mn = get("min")
+local mx = get("max")
+local err = get("err")
+local rand = get("random")
+
+btn.on_click(function()
+    print("Click!")
+    local mnvalue = tonumber(mn.get_content(), 10)
+    local mxvalue = tonumber(mx.get_content(), 10)
+
+    if mnvalue == nil or mxvalue == nil then
+        err.set_content("Enter valid numbers")
+    elseif mnvalue >= mxvalue then
+        err.set_content("Min value should be smaller than max value")
+    else
+        err.set_content()
+        local randnum = math.random(mnvalue, mxvalue)
+        rand.set_content(randnum)
+    end
+    
+end)
