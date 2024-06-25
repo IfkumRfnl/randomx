33261cf916a9ff41dbb129a06e7f7b91eb34eae1
IfkumRfnl <ifkumrfnl@gmail.com>
2024-06-25 23:44:17 +0400

    flipcoin

--- script.lua
+++ script.lua
@@ -1,8 +1,16 @@
+local err = get("err")
+
 local btn = get("btn")
 local mn = get("min")
 local mx = get("max")
-local err = get("err")
 local rand = get("random")
+
+local coinbtn = get("coinbtn")
+-- local img = get("coinimg")
+local cointype = get("cointype")
+
+local heads = "https://i.imgur.com/4SwO5Z4.png"
+local tails = "https://i.imgur.com/Lgc7Bz7.png"
 
 btn.on_click(function()
     err.set_content()
@@ -20,4 +28,18 @@ btn.on_click(function()
         rand.set_content(randnum)
     end
     
+end)
+
+
+coinbtn.on_click(function()
+    -- img.set_source("")
+    cointype.set_content("")
+    local type = math.random(1, 2)
+    if (type == 1) then
+        -- img.set_source(heads)
+        cointype.set_content("Heads")
+    else
+        -- img.set_source(tails)
+        cointype.set_content("Tails")
+    end
 end)
